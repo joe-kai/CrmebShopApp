1bc79956d504907aa2f094b2f85317c22ffaec86
JoeKai <772534263@qq.com>
2022-12-24 10:47:35 +0800

    解决分类滑动右侧，左侧到达不了底部

--- app-crmeb/module-category/src/main/java/com/joekay/module_category/ui/adapter/CategoryRightAdapter.kt
+++ app-crmeb/module-category/src/main/java/com/joekay/module_category/ui/adapter/CategoryRightAdapter.kt
@@ -1,11 +1,15 @@
 package com.joekay.module_category.ui.adapter
 
 import android.content.Context
+import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.joekay.base.adapter.BaseAdapter
+import com.joekay.base.ext.getStatusBarHeight
 import com.joekay.base.ext.load
+import com.joekay.base.ext.screenHeight
 import com.joekay.module_category.R
 import com.joekay.module_category.databinding.LayoutCategoryRightItemBinding
 import com.joekay.module_category.databinding.LayoutCategoryRightSubItemBinding
@@ -22,9 +26,30 @@ import javax.inject.Inject
 class CategoryRightAdapter @Inject constructor(
     @ActivityContext context: Context
 ) : BaseAdapter<CategoryModel>(context) {
+    override fun getItemCount(): Int {
+        return super.getItemCount() + 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == itemCount - 1) {
+            return -1
+        }
+        return super.getItemViewType(position)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        if (viewType == -1) {
+            return EmptyViewHolder()
+        }
         return ViewHolder()
+    }
+
+    inner class EmptyViewHolder : BaseViewHolder(LinearLayout(getContext())) {
+
+        override fun onBindView(position: Int) {
+            getItemView().setPadding(0, 0, 0, screenHeight - 400)
+        }
+
     }
 
     inner class ViewHolder : BaseViewHolder(R.layout.layout_category_right_item) {
